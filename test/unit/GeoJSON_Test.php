4f2a2de024fd37e56397c33828abb922628acafd
tonio <tonio@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-08 12:16:06 +0000

    [sfMapFishPlugin] initial import
    git-svn-id: http://svn.symfony-project.com/plugins/sfMapFishPlugin/branches/1.2@19018 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/GeoJSON_Test.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * This file is part of the sfMapFishPlugin package.
+ * (c) Camptocamp <info@camptocamp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @package    sfMapFishPlugin
+ * @author     Camptocamp <info@camptocamp.com>
+ */
+require_once(dirname(__FILE__).'/../bootstrap/unit.php');
+
+$t = new lime_test(13, new lime_output_color());
+
+$t->diag('GeoJSON class');
+
+try
+{
+
+  $point = new Point(0, 0);
+  $point_string = '{"type":"Point","coordinates":[0,0]}';
+  $line1 = new LineString(array(new Point(0, 0), new Point(0, 10), new Point(10, 10), new Point(10, 0), new Point(0, 0)));
+  $line1_string =  '{"type":"LineString","coordinates":[[0,0],[0,10],[10,10],[10,0],[0,0]]}';
+  $line2 = new LineString(array(new Point(1, 1), new Point(2, 5), new Point(1, 1))); // inner ring
+  $polygon = new Polygon(array($line1, $line2));
+  $polygon_string = '{"type":"Polygon","coordinates":[[[0,0],[0,10],[10,10],[10,0],[0,0]],[[1,1],[2,5],[1,1]]]}';
+  $polygon2 = new Polygon(array($line2));
+  $multipoint = new MultiPoint(array(new Point(0, 0), new Point(0, 10), new Point(10, 10)));
+  $multilinestring = new MultiLineString(array($line1, $line2));
+  $multipolygon = new MultiPolygon(array($polygon, $polygon2));
+  $geomcoll = new GeometryCollection(array(new Point(1, 10), $line));
+  $geojson = '{"type":"Feature","id":6,"geometry":{"type":"GeometryCollection","geometries":[{"type":"Point","coordinates":[1,10]},{"type":"LineString","coordinates":[[1,10],[2,15]]}]},"properties":{"name":"toto","age":20}}';
+
+}
+catch (Exception $e)
+{
+  $t->fail();
+}
+
+$t->is($point->__toString(), $point_string, 'Dumps a point');
+$t->is($line1->__toString(), $line1_string, 'Dumps a line');
+$t->is($polygon->__toString(), $polygon_string, 'Dumps a polygon');
+
+$t->is($geojson, GeoJSON::dump(GeoJSON::load($geojson)), 'Load then dump returns same GeoJSON');
+
+$t->is(get_class(GeoJSON::load($geojson)), 'Feature', 'Correctly load into Feature');
+$t->is(GeoJSON::load($geojson)->getProperties(), array('name'=>'toto', 'age'=>20), 'Correctly load properties');
+$t->is(GeoJSON::load($geojson)->getId(), 6, 'Correctly load Feature id');
+$t->is(get_class(GeoJSON::load($geojson)->getGeometry()), 'GeometryCollection', 'Correctly load Feature geometry');
+
+$geojson = '{"type":"Feature","id":6,"geometry":null, "properties": {}}';
+$t->is(get_class(GeoJSON::load($geojson)), 'Feature', 'Load allows null Geometry');
+
+$geojson = '{"type":"Feature","id":null,"geometry": {"type":"Point","coordinates":[0,0]}, "properties": {}}';
+$t->is(get_class(GeoJSON::load($geojson)), 'Feature', 'Load allows null id');
+
+$geojson = '{"type":"Feature","geometry":null, "properties": {}}';
+$t->is(get_class(GeoJSON::load($geojson)), 'Feature', 'Load allows missing id');
+
+try {
+  $geojson = '{"type":"Feature","id":6,"geometry":null}';
+  GeoJSON::load($geojson);
+  $t->fail('Properties in Feature should be mandatory');
+} catch (Exception $e) {
+  $t->pass('Propeties in Feature in Feature are mandatory');
+}
+
+try {
+  $geojson = '{"id":6,"geometry":null, "properties": {}}';
+  GeoJSON::load($geojson);
+  $t->fail('Type in Feature should be mandatory');
+} catch (Exception $e) {
+  $t->pass('Type in Feature is mandatory');
+}
+
+
+
---
+++ test/unit/WKT_Test.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * This file is part of the sfMapFishPlugin package.
+ * (c) Camptocamp <info@camptocamp.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @package    sfMapFishPlugin
+ * @author     Camptocamp <info@camptocamp.com>
+ */
+require_once(dirname(__FILE__).'/../bootstrap/unit.php');
+
+$t = new lime_test(21, new lime_output_color());
+
+$t->diag('WKT class');
+
+$null = WKT::load('invalid');
+$t->is($null, null, 'Invalid WKT returns null value');
+
+$null = WKT::load('invalid(10, 10)');
+$t->is($null, null, 'Invalid WKT returns null value');
+
+$point = WKT::load('POINT (10 11)');
+$t->is(get_class($point), 'Point', 'Loads a point into a Point object');
+$t->is($point->getX(), 10, 'Loads a correct X coordinates');
+$t->is($point->getY(), 11, 'Loads a correct Y coordinates');
+
+$multipoint = WKT::load('MULTIPOINT(10 10, 20 20)');
+$t->is(get_class($multipoint), 'MultiPoint', 'Loads a point into a MultiPoint object');
+
+$linestring = WKT::load('LINESTRING( 10 10, 20 20, 30 40)');
+$t->is(get_class($linestring), 'LineString', 'Loads a point into a LineString object');
+
+$multilinestring = WKT::load('MULTILINESTRING(( 10 10, 20 20, 30 40), ( 15 16, 21 22, 35 45))');
+$t->is(get_class($multilinestring), 'MultiLineString', 'Loads a point into a MultiLineString object');
+
+$polygon = WKT::load('POLYGON ((10 10, 10 20, 20 20, 20 15, 10 10))');
+$t->is(get_class($polygon), 'Polygon', 'Loads a point into a Polygon object');
+
+$polygon2 = WKT::load('POLYGON ((10 10, 10 20, 20 20, 20 15, 10 10), (10 11, 10 21, 20 21, 20 15, 10 11))');
+$t->is(get_class($polygon2), 'Polygon', 'Loads a polygon with internal ring into a Polygon object');
+$t->is(count($polygon2->getComponents()), 2, 'Polygon with internal ring has correct count of components');
+
+$multipolygon = WKT::load('MULTIPOLYGON(((10 10, 10 20, 20 20, 20 15, 10 10)),((60 60, 70 70, 80 60, 60 60)))');
+$t->is(get_class($multipolygon), 'MultiPolygon', 'Loads a multipolygon into a MultiPolygon object');
+
+$geometrycollection = WKT::load('GEOMETRYCOLLECTION(POINT (10 10), POINT(30 30), LINESTRING(15 15, 20 20))');
+$t->is(get_class($geometrycollection), 'GeometryCollection', 'Loads a geometrycollection into a GeometryCollection object');
+
+
+$t->is(WKT::dump($point), 'POINT(10 11)', 'Dumps a Point');
+
+$t->is(WKT::dump($multipoint), 'MULTIPOINT(10 10,20 20)', 'Dumps a MultiPoint');
+
+$t->is(WKT::dump($linestring), 'LINESTRING(10 10,20 20,30 40)', 'Dumps a LineString');
+
+$t->is(WKT::dump($multilinestring), 'MULTILINESTRING((10 10,20 20,30 40),(15 16,21 22,35 45))', 'Dumps a MultiLineString');
+
+$t->is(WKT::dump($polygon), 'POLYGON((10 10,10 20,20 20,20 15,10 10))', 'Dumps a Polygon');
+
+$t->is(WKT::dump($polygon2), 'POLYGON((10 10,10 20,20 20,20 15,10 10),(10 11,10 21,20 21,20 15,10 11))', 'Dumps a Polygon');
+
+$t->is(WKT::dump($geometrycollection), 'GEOMETRYCOLLECTION(POINT(10 10),POINT(30 30),LINESTRING(15 15,20 20))', 'Dumps a GeometryCollection');
+
+class Invalid extends Geometry { public function getCoordinates(){return 1 ;}};
+
+$null = new invalid();
+$t->is(WKT::dump($null), null, 'Unknown geometry are duymped to null');
+
+
+
+
+
+
+
